1d3fc568825405e01db125ac7450d8dc6cb27710
Floris Jousselin <floris.jousselin@NEURA-3627.nad.neura-robotics.com>
2025-06-25 16:42:14 +0200

    fix: docker environment issue using local submodule

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 set -e
 
+cd ~/ros2_ws/src/HARP2_ROS/ydlidar/YDLidar-SDK
+
 # Build YDLidar-SDK
-cd /ros_ws/src/HARP2_ROS/ydlidar/YDLidar-SDK
+echo "Contenu du dossier courant au début :"
+ls
+#cd src/HARP2_ROS/ydlidar/YDLidar-SDK
+[ -d build ] && sudo rm -r build
 mkdir -p build
 cd build
 cmake ..
 make
 sudo make install
 
-cd /ros_ws
+cd ~/ros2_ws
 
+sudo apt update
 # Init rosdep si ce n’est pas déjà fait
 sudo rosdep init 2>/dev/null || true
 rosdep update
 
 # Install deps
-rosdep install --from-paths src --ignore-src -r -y
+# rosdep install --from-paths ~/ros2_ws/src --ignore-src --skip-keys="gazebo libgazebo-dev ros-humble-warehouse-ros-mongo" -r -y
 
 # Build
 . /opt/ros/humble/setup.sh && \
 colcon build --symlink-install
 
-# Source automatique
-echo "source /ros_ws/install/setup.bash" >> ~/.bashrc
-
+# Automatic sourcing
+echo "source ~/ros2_ws/install/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
